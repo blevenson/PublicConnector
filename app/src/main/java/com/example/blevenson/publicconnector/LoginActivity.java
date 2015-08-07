10d93669a9a392085948614eeb891bd1f4eacf6d
Blevenson <blevenson68@gmail.com>
2015-08-07 14:23:40 -0700

    Initial commit

---
+++ app/src/main/java/com/example/blevenson/publicconnector/LoginActivity.java
@@ -0,0 +1,101 @@
+package com.example.blevenson.publicconnector;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Spinner selector;
+    private Button joinButton;
+    private TextView message;
+    private EditText userName;
+
+    private String chatRoom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        selector = (Spinner)findViewById(R.id.roomSelector);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.chatroom_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selector.setAdapter(adapter);
+
+
+        selector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                chatRoom = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        userName = (EditText)findViewById(R.id.userName);
+        message = (TextView)findViewById(R.id.loginMessage);
+
+        joinButton = (Button)findViewById(R.id.joinButton);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userName.getText().toString().trim().length() <= 0) {
+                    message.setTextColor(Color.RED);
+                    message.setTextSize(20);
+                    message.setText("Please enter a user name");
+                    return;
+                }else{
+                    message.setTextColor(Color.BLACK);
+                    message.setTextSize(50);
+                    message.setText("Login");
+                }
+
+                if(chatRoom != null) {
+                    //Move to main activty
+                    moveToMainActivity();
+                }
+            }
+        });
+    }
+
+    private void moveToMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("userName", userName.getText().toString());
+        intent.putExtra("chatRoom", chatRoom);
+        startActivity(intent);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/blevenson/publicconnector/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.blevenson.publicconnector;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Firebase myFirebaseRef;
+
+    private String userName;
+    private String chatRoom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        userName = intent.getStringExtra("userName");
+        chatRoom = intent.getStringExtra("chatRoom");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final EditText messageText = (EditText)findViewById(R.id.messageText);
+        final TextView messanger = (TextView)findViewById(R.id.display);
+        messanger.setMovementMethod(new ScrollingMovementMethod());
+
+        myFirebaseRef = new Firebase("https://publicconnector.firebaseio.com/").child(chatRoom);
+
+        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                messanger.setText("");
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Chat chatter = child.getValue(Chat.class);
+                    messanger.append(chatter.getAuthor() + ": " + chatter.getMessage() + "\n");
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        messageText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER && messageText.getText().toString().trim().length() > 0) {
+                    if(messageText.getText().charAt(0) == '/'){
+                        //Command
+                        if(messageText.getText().toString().toLowerCase().contains("clear"))
+                            messanger.setText("");
+                        else if(messageText.getText().toString().toLowerCase().contains("text") &&
+                                messageText.getText().toString().toLowerCase().contains("color"))
+                            try {
+                                messanger.setTextColor(Color.parseColor(messageText.getText().toString().substring(12).toUpperCase()));
+                            }catch (Exception e){}
+                        else
+                            messanger.append("[That is not a valid command]");
+                    }else
+                        myFirebaseRef.push().setValue(new Chat(messageText.getText().toString(), userName));
+
+                    messageText.setText("");
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
